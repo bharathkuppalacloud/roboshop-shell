151401c5ec3edd03637193e8b790417665820c5e
Bharath kumar kuppala <bharath@simplifyvms.com>
2024-08-23 14:20:00 +0530

    catalogue service

---
+++ catalogue.sh
@@ -0,0 +1,16 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app || return
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+# change catalogue to service vim /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+#add client address /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y
