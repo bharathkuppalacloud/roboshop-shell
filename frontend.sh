3d2e6814e9f4e413c444587a971687cfb0a38789
Bharath kumar kuppala <bharath@simplifyvms.com>
2024-09-03 22:22:43 +0530

    moved copy command file to top since there is change directory going down

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,7 @@
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
-
+cp nginx.conf /etc/nginx/nginx.conf
 systemctl enable nginx
 systemctl start nginx
 
@@ -11,5 +11,5 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v
 
 cd /usr/share/nginx/html || return
 unzip /tmp/frontend.zip
-cp nginx.conf /etc/nginx/nginx.conf
+
 systemctl restart nginx
