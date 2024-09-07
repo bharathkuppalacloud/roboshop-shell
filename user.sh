3950cb5e5621e16c9ef9ce5f5faf0584ccce1ef5
Bharath kumar kuppala <bharath@simplifyvms.com>
2024-09-07 15:53:06 +0530

    user.sh config

--- user.sh
+++ user.sh
@@ -2,7 +2,6 @@ cp user.service /etc/systemd/system/user.service
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
-useradd roboshop
 mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
 cd /app || return
