804b3a88bde71cb0a08a8d60fcb69312551aa015
Bharath kumar kuppala <bharath@simplifyvms.com>
2024-09-11 11:28:27 +0530

    cart.sh config

--- cart.sh
+++ cart.sh
@@ -6,7 +6,7 @@ useradd roboshop
 rm -rf /app
 mkdir "app"
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
-cd /app
+cd /app || return
 unzip /tmp/cart.zip
 cd /app
 npm install
