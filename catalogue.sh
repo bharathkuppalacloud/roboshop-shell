643b4e5c335534c9b9f2e8a951bca44640387a75
Bharath kumar kuppala <bharath@simplifyvms.com>
2024-09-13 13:26:05 +0530

    Dry for all other services

--- catalogue.sh
+++ catalogue.sh
@@ -1,18 +1,6 @@
 source utils.sh
-cp catalogue.service /etc/systemd/system/catalogue.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+component=catalogue
 NODEJS
-useradd roboshop
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app || return
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-# change catalogue to service vim /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
 #add client address /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
 mongosh --host mongo.dev.codedeploywithbharath.tech</app/db/master-data.js
