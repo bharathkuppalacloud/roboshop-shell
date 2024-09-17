b77777fbb8b85245fd26ede229181df3581bf42b
Bharath kumar kuppala <bharath@simplifyvms.com>
2024-09-17 23:10:07 +0530

    dry

--- utils.sh
+++ utils.sh
@@ -4,19 +4,20 @@ rm -f $LOG_FILE
 SUCCESS() {
   # shellcheck disable=SC1073
   if [ $1 -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m" $?
-  else
-    echo  -e "\e[31mSUCCESS\e[0m"
-    exit $1 # stop if something failed
-    echo
-    echo Refer the log file at /tmp/roboshop-shell.log
-  fi
+      echo -e "\e[32mSUCCESS\e[0m"
+    else
+      echo -e "\e[31mFAILURE\e[0m"
+      echo
+      echo "Refer the log file for more information : File Path : ${LOG_FILE}"
+      exit $1
+    fi
 }
 
 PRINT() {
-  echo &>>LOG_FILE
-  echo "************************ $* ##################" &>>$LOG_FILE
-  echo $*
+  echo &>>$LOG_FILE
+    echo &>>$LOG_FILE
+    echo " ####################################### $* ########################################" &>>$LOG_FILE
+    echo $*
 }
 
 NODEJS(){
